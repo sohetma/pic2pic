d3d569d14fba2bedd17f683e332635a7b3992f8d
Benoît Moëns <benoit.moens@hotmail.com>
2019-10-30 15:34:38 +0100

    header

---
+++ src/BoutonStart.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const ButtonStart = () => {
+    return(
+    <button style = {{ fontWeight : 'bold', fontSize : '40px', borderRadius: '30px'}}>Start</button>
+);
+    }
+export default ButtonStart;
---
+++ src/Round.js
@@ -0,0 +1,16 @@
+import React from 'react'
+const Round = () =>{
+return (
+        <label>
+          Pick your favorite flavor:
+            <option value="grapefruit">Grapefruit</option>
+            <option value="lime">Lime</option>
+            <option value="coconut">Coconut</option>
+            <option value="mango">Mango</option>
+
+        </label>
+        <input type="submit" value="Submit" />
+    );
+}
+    export default Round
+
