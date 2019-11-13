53b48d5922f8c3efa053daae646e689e96c19ad4
Sohet Maxime <maxime.sohet.ms@gmail.com>
2019-11-13 18:03:37 +0100

    problems solved

--- src/components/drawer/PopupPics.js
+++ src/components/drawer/PopupPics.js
@@ -1,7 +1,7 @@
 import React , { Component } from 'react';
 import './popup.css';
 import Popup from "reactjs-popup";
-import APP_ACCESS_KEY from './token.js';
+// import APP_ACCESS_KEY from './token.js';
 const axios = require('axios');
 
 
@@ -38,7 +38,7 @@ class Popupic extends Component {
     const response = await axios.get('https://api.unsplash.com/search/photos', {
         params: { query: query},
         headers: {
-            Authorization: APP_ACCESS_KEY
+            Authorization: 'Client-ID 95a56e4dbdbda45b38ebd65bfde2e487cc2b6afbb0a6fd9737d915f36d872a9b'
         }
     })
 
@@ -73,11 +73,11 @@ class Popupic extends Component {
     return (
       <div className="modal-pop">
         <Popup
-          className="popup3pics"
           open={this.state.open}
           closeOnDocumentClick
           onClose={this.closeModal}
         >
+        <div>
           <h1> Choose a picture </h1>
           <div className="modal images-popup">
             <img src={this.state.photos[0]} alt="image1-popup" id={"pop-image-unsplash"} className="close-popup image-unsplash" onClick={() => this.selectionPicture(this.state.photos[0])} />
@@ -85,6 +85,7 @@ class Popupic extends Component {
             <img src={this.state.photos[2]} alt="image3-popup" className="close-popup image-unsplash" onClick={() => this.selectionPicture(this.state.photos[2])} />
           </div>
           <h2> {this.props.word} </h2>
+        </div>
         </Popup>
       </div>
     );
