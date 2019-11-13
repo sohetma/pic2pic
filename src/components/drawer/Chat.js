a6bcf16e64cebb612b52dfc658f8bb89fc2b559a
Benoît Moëns <benoit.moens@hotmail.com>
2019-11-13 10:55:51 +0100

    Version avec le chat

--- src/components/drawer/Chat.js
+++ src/components/drawer/Chat.js
@@ -1,24 +1,78 @@
-import React from 'react';
+import React, { Component } from 'react';
+import InputMessage from './InputMessage';
+import AnswerBoard from './AnswerBoard';
 import './Chat.css';
 
-// Component chat : its here we chat (empty for the moment)
-class Chat extends React.Component  {
-  constructor(props){
-    super(props);
-
-    this.state ={
-      message : {}
+class Chat extends Component {
+    state = {
+        currentInput: '',
+        isWriting: false,
+        messages: []
     }
-  }
 
-  render(){
-    return (
-      <div className="Chat">
-        <section className="messages"></section>
-        <span className="tryWord"> try a word </span>
-      </div>
-    )
-  }
-};
+    handleChange = (event) => {
+        this.setState({ isWriting: true })
+        this.setState({ 
+            currentInput: event.target.value
+        });
+    }
+
+    handleSubmit = (event, messageToAdd) => {
+        this.setState({ isWriting: false })
+        if (this.state.currentInput !== '') {
+             let newMessages = this.getNewMessages();
+             this.setState({
+                 messages: newMessages
+             })
+             this.setState({currentInput: ''})
+            }
+            event.preventDefault();   
+    }
+
+    getNewMessages = () => {
+        let newDate = new Date().toLocaleString();
+        let from = this.whoTalk()
+        let existingMessages  = this.state.messages;
+        let newMessage = { content: from === "Benoit :" ? this.state.currentInput: this.state.currentInput.substring(0, this.state.currentInput.length - 5), 
+                            sender: from, 
+                            date: "On " + newDate }
+        existingMessages.push(newMessage)
+
+        return existingMessages;
+    }
+
+
+    whoTalk = () => {
+        var lastChar = this.state.currentInput.slice(-5);
+        console.log(lastChar)
+        if(lastChar==="#else") {
+            return "someone else"
+        } else {
+            return "Benoit :"
+        }
+    }
+  
+
+    render () {
+    
+        return (
+        <div>
+            <div className="chat-zone">   
+                <AnswerBoard
+                    messages={this.state.messages}>
+                </AnswerBoard>
+                { this.state.isWriting ? "" : ""} 
+            </div>
+            <div>
+                <InputMessage type="text" 
+                            addMessage={this.handleChange}
+                            confirmMessage={this.handleSubmit}
+                            currentMessage={this.state.currentInput}>
+                </InputMessage>
+        </div>
+        </div>
+        )   
+    }
+}
 
 export default Chat;
